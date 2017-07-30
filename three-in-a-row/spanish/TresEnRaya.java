1db02220925623c6406ce25a8452995622aebf3d
Carlos Fernández <cfernandez@Carloss-MacBook-Pro.local>
2017-07-30 23:17:19 +0200

    refactor v1

--- three-in-a-row/spanish/TresEnRaya.java
+++ three-in-a-row/spanish/TresEnRaya.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class TresEnRaya {
-  char[][] tablero = new char[5][5];
+  char[][] tablero;
 
   Jugador jugadorCruz;
   Jugador jugadorCirculo;
@@ -9,22 +10,25 @@ public class TresEnRaya {
   boolean partidaTerminada = false;
   boolean juegoTerminado = false;
 
-  public TresEnRaya(){
+  public TresEnRaya(int filas, int columnas){
+    this.tablero = new char[filas][columnas];
+
     this.jugadorCruz = new Jugador('X');
     this.jugadorCirculo = new Jugador('O');
   }
 
   public static void main(String[] args) {
-    TresEnRaya juego = new TresEnRaya();
+    int filas = 8;
+    int columnas = 8;
+
+    TresEnRaya juego = new TresEnRaya(filas, columnas);
     Scanner scanner = new Scanner(System.in);
-    juego.obtenerNombres(scanner);
+    juego.obtenerJugadores(scanner);
 
     while(!juego.juegoTerminado){
       juego.nuevaPartida(scanner);
-      boolean quierenSeguir = juego.jugadoresQuierenSeguir(scanner);
-      if(quierenSeguir){
-        juego.partidaTerminada = false;
-        juego.tablero = new char[5][5];
+      if(juego.jugadoresQuierenSeguir(scanner)){
+        juego.reiniciarTablero(filas, columnas);
       }else{
         juego.juegoTerminado = true;
       };
@@ -42,9 +46,8 @@ public class TresEnRaya {
       this.pintarTablero();
 
       this.nuevoTurno(scanner, this.jugadorCirculo);
-      System.out.println("Así queda el tablero:");
-      this.pintarTablero();
 
+      //Si el jugador de circulos tiene tres en raya, el jugador de cruces solo podría empatar, pero la ronda ya ha terminado
       if(this.jugadorCirculo.tieneTresEnRaya){
         System.out.println(this.jugadorCirculo.nombre + " tiene tres en raya, veamos si puedes empatar, " +
         this.jugadorCruz.nombre + "!");
@@ -52,17 +55,19 @@ public class TresEnRaya {
       }
 
       this.nuevoTurno(scanner, this.jugadorCruz);
-      System.out.println("Así queda el tablero:");
-      this.pintarTablero();
 
+      //Si el jugador de cruces tiene tres en raya
       if(this.jugadorCruz.tieneTresEnRaya){
+        //Y el de circulos también, hay empate
         if(this.jugadorCirculo.tieneTresEnRaya){
           System.out.println("Tenemos un empate!");
+        //Y el de circulos no, gana el de cruces
         }else{
           System.out.println("Enhorabuena " + this.jugadorCruz.nombre + ", has ganado!");
           this.jugadorCruz.partidasGanadas += 1;
         }
         this.partidaTerminada = true;
+      //Si el jugador de cruces no tiene tres en raya, pero el de circulos sí, es el ganador.
       }else if(this.jugadorCirculo.tieneTresEnRaya){
         System.out.println("Enhorabuena " + this.jugadorCirculo.nombre + ", has ganado!!");
         this.jugadorCirculo.partidasGanadas += 1;
@@ -70,12 +75,6 @@ public class TresEnRaya {
     }
   }
 
-  public boolean jugadoresQuierenSeguir(Scanner scanner){
-    System.out.println("Queréis seguir jugando? 0=No/1=Sí");
-    int siguen = scanner.nextInt();
-    return siguen == 1;
-  }
-  
   public void nuevoTurno(Scanner scanner, Jugador jugador) {
     boolean seHaPintadoLaCasilla = false;
 
@@ -86,13 +85,24 @@ public class TresEnRaya {
       if(this.esUnaCasillaValida(fila, columna)){
         this.pintarCasilla(jugador.simbolo, fila, columna);
         seHaPintadoLaCasilla = true;
+        //Después de pintar la casilla comprueba si el jugador tiene Tres en raya
         this.comprobarFilas(jugador);
         this.comprobarColumnas(jugador);
       }else{
         System.out.println("Vuelve a intentarlo");
       }
     }
-   
+
+    System.out.println("Así queda el tablero:");
+    this.pintarTablero();
+  }
+
+  public void reiniciarTablero(int filas, int columnas){
+      this.tablero = new char[filas][columnas];
+
+      this.partidaTerminada = false;
+      this.jugadorCirculo.tieneTresEnRaya = false;
+      this.jugadorCruz.tieneTresEnRaya = false;
   }
 
   public boolean esUnaCasillaValida(int fila, int columna){
@@ -153,21 +163,10 @@ public class TresEnRaya {
     }
   }
 
-  public void comprobarDiagonalesDeIzquierdaADerecha(Jugador jugador) {
-  }
+  //TODO
+  public void comprobarDiagonalesDeIzquierdaADerecha(Jugador jugador) {}
+  //TODO
   public void comprobarDiagonalesDeDerechaAIzquierda(char simbolo) {}
-
-  public void obtenerNombres(Scanner scanner) {
-    System.out.println("Introduce el nombre del jugador que irá a circulos");
-    // this.jugadorCirculo.nombre = scanner.nextLine();
-    this.jugadorCirculo.nombre = "Mario";    
-
-    System.out.println("Introduce el nombre del jugador que irá a cruces");
-    // this.jugadorCruz.nombre = scanner.nextLine();
-    this.jugadorCruz.nombre = "Carlos";
-
-    System.out.println("OK!, suerte " + jugadorCirculo.nombre + " y " + jugadorCruz.nombre + "!\n");
-  }
 
   public void pintarTablero() {
     //Primera línea con los índices de columnas
@@ -190,6 +189,26 @@ public class TresEnRaya {
       System.out.println();
     }
     System.out.println("\n");
+  }
+
+  public void obtenerJugadores(Scanner scanner) {
+    System.out.println("Introduce el nombre del jugador que irá a circulos");
+    //Comentado para probar rápidamente
+    // this.jugadorCirculo.nombre = scanner.nextLine();
+    this.jugadorCirculo.nombre = "Mario";    
+
+    System.out.println("Introduce el nombre del jugador que irá a cruces");
+    //Comentado para probar rápidamente
+    // this.jugadorCruz.nombre = scanner.nextLine();
+    this.jugadorCruz.nombre = "Carlos";
+
+    System.out.println("OK!, suerte " + jugadorCirculo.nombre + " y " + jugadorCruz.nombre + "!\n");
+  }
+
+  public boolean jugadoresQuierenSeguir(Scanner scanner){
+    System.out.println("Queréis seguir jugando? 0=No/1=Sí");
+    int siguen = scanner.nextInt();
+    return siguen == 1;
   }
 
 }
